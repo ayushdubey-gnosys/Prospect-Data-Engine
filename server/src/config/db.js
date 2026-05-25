2e02a0d79625e305ce4bccc43728c4c20e3c64c4
ayushdubey-gnosys <ayush.dubey@gnosys.com>
2026-05-25 18:08:59 +0530

    mongo

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
